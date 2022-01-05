b01695e33b333d387f001b0ca58dee0c912ec82b
MCaliJr <MartinK.MoC@gmail.com>
2022-01-05 18:16:32 +0100

    Added actors data fetch also

--- fetch-content.mjs
+++ fetch-content.mjs
@@ -1,13 +1,14 @@
 import fs from 'fs';
 import fetch from 'node-fetch';
 
+// Movie names from DB
 let movieNames = [];
 
-let pageNumber = 1;
-while (pageNumber < 10) {
-  pageNumber++;
+let moviesPageNumber = 1;
+while (moviesPageNumber < 10) {
+  moviesPageNumber++;
   const response = await fetch(
-    `https://api.themoviedb.org/3/movie/top_rated?api_key=5494c3aa84c3dc8ee3abdbca31b17bd6&language=en-US&page=${pageNumber}`
+    `https://api.themoviedb.org/3/movie/top_rated?api_key=5494c3aa84c3dc8ee3abdbca31b17bd6&language=en-US&page=${moviesPageNumber}`
   );
   const data = await response.json();
   const names = data.results.map((movie) => movie.title);
@@ -19,3 +20,23 @@ movieNames = movieNames.filter((name) => name.length < 30);
 
 // Save to file
 fs.writeFileSync('./src/data/movieNames.json', JSON.stringify(movieNames));
+
+//
+// Actors names from DB
+let actorsNames = [];
+
+let actorsPageNumber = 1;
+while (actorsPageNumber < 10) {
+  actorsPageNumber++;
+  const response = await fetch(
+    `https://api.themoviedb.org/3/person/popular?api_key=5494c3aa84c3dc8ee3abdbca31b17bd6&language=en-US&page=${actorsPageNumber}`
+  );
+  const data = await response.json();
+  const names = data.results.map((actor) => actor.name);
+  actorsNames = actorsNames.concat(names);
+}
+
+actorsNames = actorsNames.filter((name) => name.length < 30);
+
+// Save to file
+fs.writeFileSync('./src/data/actorsNames.json', JSON.stringify(actorsNames));
