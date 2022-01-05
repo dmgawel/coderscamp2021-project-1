ee2f66421d04ca281d63a1df42970ed039148070
MCaliJr <88389986+MCaliJr@users.noreply.github.com>
2022-01-05 18:16:13 +0100

    Cleaned mainJs (#99)

--- src/main.js
+++ src/main.js
@@ -18,6 +18,7 @@ import { movieFramesData } from './pages/frames';
 import soundsData from './pages/sounds';
 
 import movieNames from './data/movieNames.json';
+
 console.log(movieNames);
 
 // Hamburger navigation
@@ -147,65 +148,6 @@ function nextQuestion() {
   }
 }
 
-/**
- * Logika
- *
- * Po wybraniu quizu ustawiany stan na podstawie danych o quizie
- */
-
-// Array that will store invalid movie frame answers
-let wrongAnswers = [];
-
-// async function getDataFromDb(pageNumber) {
-//   const response = await fetch(
-//     `https://api.themoviedb.org/3/movie/top_rated?api_key=e0add4835a4a4c34f08aeb4c32425f01&language=en-US&page=${pageNumber}`
-//   );
-//   const data = await response.json();
-//   return data;
-// }
-
-// ! fetch commented out
-// // Getting info from API for movie names
-// async function loadWrongMovieNames() {
-//   // Making 8 different calls to API because single one contains only 20 movie names
-//   for (let i = 1; i <= 8; i++) {
-//     // Making sure that every API call returns data, otherwise showing error image and message
-//     // eslint-disable-next-line no-await-in-loop
-//     const data = await getDataFromDb(i).catch((e) => {
-//       questionElement.innerHTML = 'Sorry, our database could not be reached, refresh the page and try another quiz :)';
-//       frameImg.src = './src/movieFrames/error.jpeg';
-//       answerElements.forEach((answerElement) => {
-//         // eslint-disable-next-line no-param-reassign
-//         answerElement.style.display = 'none';
-//       });
-//       return e;
-//     });
-//     // Filtering array so it contains only movies in English
-//     let wrongAnswersFromThisLoop = data.results.map((arr) => {
-//       if (arr.original_language === 'en') {
-//         return arr.original_title;
-//       }
-//     });
-
-//     // Getting rid of all non-English deleted movies turned to "undefined"
-//     wrongAnswersFromThisLoop = wrongAnswersFromThisLoop.filter((x) => x !== undefined);
-
-//     // Adding current API call to overall wrongAnswers array
-//     wrongAnswers = wrongAnswers.concat(wrongAnswersFromThisLoop);
-//   }
-
-//   // Randomizing wrong answers order
-//   wrongAnswers = wrongAnswers.sort(() => Math.random() - 0.5);
-
-//   // Function returns array with invalid movie names
-//   return wrongAnswers;
-// }
-// loadWrongMovieNames().catch((e) => {
-//   // eslint-disable-next-line no-unused-expressions
-//   `We were not able to load this quiz for You because of error: ${e}`;
-//   throw new Error(`Error outside of the function! ${e}`);
-// });
-
 function deselectInputs() {
   answerElements.forEach((answerEl) => {
     answerEl.checked = false;
@@ -233,21 +175,6 @@ function loadQuiz() {
   c_text.innerHTML = loadQuizQuizData.c;
   d_text.innerHTML = loadQuizQuizData.d;
 
-  // // Making a, b, c, d options display as the correct answer or random wrong answer + making sure that displayed names are not repeating
-  // a_text.innerHTML = loadQuizQuizData.a || wrongAnswers[Math.floor(Math.random() * 91)];
-  // do {
-  //   b_text.innerHTML = loadQuizQuizData.b || wrongAnswers[Math.floor(Math.random() * 91)];
-  // } while (b_text.innerHTML === a_text.innerHTML);
-  // do {
-  //   c_text.innerHTML = loadQuizQuizData.c || wrongAnswers[Math.floor(Math.random() * 91)];
-  // } while (c_text.innerHTML === a_text.innerHTML || c_text.innerHTML === b_text.innerHTML);
-  // do {
-  //   d_text.innerHTML = loadQuizQuizData.d || wrongAnswers[Math.floor(Math.random() * 91)];
-  // } while (
-  //   d_text.innerHTML === a_text.innerHTML ||
-  //   d_text.innerHTML === b_text.innerHTML ||
-  //   d_text.innerHTML === c_text.innerHTML
-  // );
   // Randomizing order of ABCD answers
   for (let i = ul.children.length; i >= 0; i--) {
     // eslint-disable-next-line no-bitwise
@@ -395,9 +322,3 @@ navLinks.forEach((el) => {
     // console.log(quizData[name]);
   });
 });
-
-/* ! do soundtrack musimy zająć się:
-klasa soundtracks-question-elements - zdjąć hidden
-
-
-*/
