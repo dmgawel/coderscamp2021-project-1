51a8649a6861e236ee8bfea1ee0c0b7a6c32bef9
Dawid Gaweł <d.gawel@livechatinc.com>
2022-01-05 17:29:37 +0100

    add content fetching from api

--- fetch-content.mjs
+++ fetch-content.mjs
@@ -1,8 +1,21 @@
 import fs from 'fs';
-
 import fetch from 'node-fetch';
 
-const response = await fetch('https://github.com/');
-const body = await response.text();
+let movieNames = [];
 
-fs.writeFileSync('./text.txt', body);
+let pageNumber = 1;
+while (pageNumber < 10) {
+  pageNumber++;
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/top_rated?api_key=5494c3aa84c3dc8ee3abdbca31b17bd6&language=en-US&page=${pageNumber}`
+  );
+  const data = await response.json();
+  const names = data.results.map((movie) => movie.title);
+  movieNames = movieNames.concat(names);
+}
+
+// Filter out long titles
+movieNames = movieNames.filter((name) => name.length < 30);
+
+// Save to file
+fs.writeFileSync('./src/data/movieNames.json', JSON.stringify(movieNames));
